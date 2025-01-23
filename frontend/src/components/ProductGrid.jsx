52d4ba31337d6fa1ce075c5dafe18b09b1fb4171
Abhishek-Marethiya <168818112+Abhishek-Marethiya@users.noreply.github.com>
2025-01-23 13:54:47 +0530

    urls

--- frontend/src/components/ProductGrid.jsx
+++ frontend/src/components/ProductGrid.jsx
@@ -16,7 +16,7 @@ function ProductGrid({filters ,onResultCountUpdate}) {
       }).toString();
   try {
      setIsLoading(true);
-      const response = await fetch(`http://localhost:5000/api/products?${query}`);
+      const response = await fetch(`https://fog-ui-development-task-backend-5llaip9oo.vercel.app/api/products?${query}`);
       const data = await response.json();
       setProducts(data.products);
       setTotal(data.total);
